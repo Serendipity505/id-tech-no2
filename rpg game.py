1a50efc18e27ecd6d402ba3a09011f53680f60df
Serendipity505 <xuanhua30@midlandps.org>
2025-07-09 08:34:30 -0700

    second commit

---
+++ rpg game.py
@@ -0,0 +1,43 @@
+# The following program crwate a text adventure game.
+    #Introduction
+print("\n")
+print("                                        I Accidentally Conquered the Final Boss of the Royal Family Game")
+print("                                                               RPG Game By Emily")
+print("\n       Welcome accommodation, to the game world: “Fantasia”. I am your Game Threading System, Q. "
+          "\nIn order to travel back to your universe, you must finish all the missions with the main character; Sir Charles and defeat the finale boss. "
+          "\nIt is my job to accompany you. Any OOC or OTP may lead to xp loss and eventually ending the game."
+          "\nGood luck accommodation and may the odds ever be in your favor. (lol)")
+
+print("\n     You wonder wth that was about. As you look around, you find yourself by a Palace, and it seems like you are lying in front of the Palace’s main entrance."
+          "\nBy your side, there is a badge with your face and name imprinted on it. "
+          "\nYou then found a book in your pocket. “Fantasia Game Plot” is its title. You wondered huh so this is the game I traveled into."
+      "\n"
+      "\nThe system gave you a few action choices......:")
+
+#player choices
+print("\n1)Run away! There’s no F-ing way this is all real!")
+print("2)Pick up the badge and opens the Palace doors. The system said if I want to escape, I must complete the missions!")
+print("3)Nap. Zzzzzzzzzzzz……")
+player_choices = int(input("Please choose wisly my accommpdation:  "))
+
+if player_choices == 1:
+    print("\n You scrambles to get up and start sprinting towards the other direction. Sadly, you got sapped by lighting before you got far."
+          "\n You the found yourself in front of the Palace again.")
+
+elif player_choices == 2:
+    print("\n You gathered up your courage and steps into the hollow hall. "
+          "\nEvery thing seems so grand and luxurious. Walls covered in Gold, mystical paintings on the cieling.... "
+          "\nThe hallway seems to go on forever....")
+
+    # riddle
+    print("\nAs you strode through the empty hall, a gaurd jumped in your way...."
+          "\n'Ugh great! A NPC' you though")
+
+elif player_choices == 3:
+    print("\nGeT oUt!!!!!!!!! Ur NoT sKiBiDi!!!!!!! Ur Not ThE AlPhA!!!!!!!!!!!! U hAvE ZeRo RiZzZZZZzZzzzzZZZz!!")
+
+
+adventure()
+
+
+
