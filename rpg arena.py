cff60df911a1ae984294fc4495bc98a0772289d0
Serendipity505 <xuanhua30@midlandps.org>
2025-07-10 11:28:49 -0700

    rpg game no2

--- rpg arena.py
+++ rpg arena.py
@@ -3,15 +3,32 @@
 
 class Character:
     # Constructor
-    def __init__(self, name, strength, health, defense):
+    def __init__(self, name, strength, health, defense,speed,intelligence):
         self.name = name
         self.strength=strength
         self.health=health
         self.defense = defense
+        self.speed = speed
+        self.intelligence = intelligence
 
-player = Character("Momo the Cat", 100,50,100)
+    #take_damage() Method
+    def take_damage(self,damage):
+        damage_taken = damage - self.defense
+        self.health-=damage_taken
+        return damage_taken
+    #attack() Metgod
+    def attack(self,target):
+        damage = self.strength * 2
+        damage_dealt = target.take_damage(damage)
+        return damage_dealt
+    # is_alive() Method
+    def is_alive(self):
+        return self.health > 0
+player = Character("Momo the Cat", 30,50,100,100,100)
 print(player.name)
 print(player.strength)
 print(player.health)
 print(player.defense)
+print(player.speed)
+print(player.intelligence)
 
